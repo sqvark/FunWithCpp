f1646539eef2655b5bcad28e53d4a975594efcc3
Alexander <90461796+sqvark@users.noreply.github.com>
2022-04-26 13:40:51 +0300

    separate cases

--- delegating_ctor.cpp
+++ delegating_ctor.cpp
@@ -1,3 +1,7 @@
+
+// Question: if constructor of object throws exception
+// is destructor will be called?
+
 #include <iostream>
 #include <string>
 #include <stdexcept>
@@ -10,34 +14,21 @@ const std::string BAD_ARG_STR = "42";
 class Printer
 {
 public:
-    Printer()
-    {
-        PrintCall(__FUNCTION__);
-    }
-
     Printer(int id): id_(id)
     {
         PrintCall(__FUNCTION__);
 
-        if (id == BAD_ARG_ID) {
-            throw std::invalid_argument("Bad arg id");
-        }
+        if (id == BAD_ARG_ID) throw std::invalid_argument("Bad arg id");
     }
 
     Printer(const std::string& s): Printer(23)
     {
-        cout << "Printer[" << id_ << "]::"
-             << __FUNCTION__ << "(" << s << ")\n";
+        cout << "Printer(\"" << s << "\")\n";
 
-        if (s == BAD_ARG_STR) {
-            throw std::invalid_argument("Bad arg string");
-        }
+        if (s == BAD_ARG_STR) throw std::invalid_argument("Bad arg string");
     }
 
-    ~Printer()
-    {
-        PrintCall(__FUNCTION__);
-    }
+    ~Printer() { PrintCall(__FUNCTION__); }
 
 private:
     int id_ {};
@@ -49,14 +40,15 @@ private:
 
 int main()
 {
+    cout << "-- A --------------------\n";
     {
-        Printer a;
+        Printer a(12);
     }
 
+    // constructor throws exception - destructor will not be called
+    cout << "-- B --------------------\n";
     try
     {
-        // exception at constructor -
-        // destructor will not be called
         Printer b(BAD_ARG_ID);
     }
     catch (const std::exception& e)
@@ -64,10 +56,11 @@ int main()
         cout << "Exception: " << e.what() << '\n';
     }
 
+    // exception at constructor, but desctructor is called -
+    // delegating constructor successfully creates object
+    cout << "-- C --------------------\n";
     try
     {
-        // exception at constructor, but desctructor is called
-        // because of using delegating constructor
         Printer c(BAD_ARG_STR);
     }
     catch (const std::exception& e)
