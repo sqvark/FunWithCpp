ed8f2fbc6efebc1f958216cfbd9e17ebe40640f2
Alexander <90461796+sqvark@users.noreply.github.com>
2023-09-08 17:10:17 +0300

    Examples added
    Add examples of designated initializers usage

--- designated_array_initializers.c
+++ designated_array_initializers.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-void print(int * arr, int len)
+void print_array(int * arr, int len)
 {
     for (int i = 0; i < len; ++i) {
         printf("%d ", arr[i]);
@@ -10,15 +10,34 @@ void print(int * arr, int len)
     printf("\n");
 }
 
+#define ARRAY_SIZE(x)    (sizeof(x) / sizeof(x[0]))
+#define PRINT(x)         (print_array(x, ARRAY_SIZE(x)))
+
 int main()
 {
     int values[10] = { [7] = 1, [2] = 2 };
-    print(values, 10);
-    // 0 0 2 0 0 0 0 1 0 0 
+    PRINT(values);
+    // 0 0 2 0 0 0 0 1 0 0
 
     int values2[10] = { [4 ... 8] = 3, [1] = 4 };
-    print(values2, 10);
+    PRINT(values2);
     // 0 4 0 0 3 3 3 3 3 0
+
+    int values3[10] = { 5 };
+    PRINT(values3);
+    // 5 0 0 0 0 0 0 0 0 0
+
+    int values4[10] = { [0 ... 9] = 6 };
+    PRINT(values4);
+    // 6 6 6 6 6 6 6 6 6 6
+
+    int values5[10] = { [4] = 1, 2 };
+    PRINT(values5);
+    // 0 0 0 0 1 2 0 0 0 0
+
+    int values6[10] = { [0 ... 4] = 1, [5 ... 9] = 2 };
+    PRINT(values6);
+    // 1 1 1 1 1 2 2 2 2 2
 
     return 0;
 }
